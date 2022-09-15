de3e7798824fe307227edefe3fe5f12b9cc5d3a9
Olya <olbka0844@gmail.com>
2022-09-15 22:29:30 +0200

    first commit

---
+++ src/lesson_2/script.js
@@ -0,0 +1,8 @@
+const MyName = 'Olya';
+console.log("MyName =", MyName );
+
+const mySurmame = 'Semenenko';
+console.log("mySurmame, =", mySurmame);
+
+const myСity = 'Kharkiv';
+console.log("myСity, =", myСity);
