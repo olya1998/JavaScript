b204d2ff7f93ec40fce9b9ba8a7a7b4cea3640ad
Olya <olbka0844@gmail.com>
2022-09-17 23:40:03 +0200

    homework lesson 5

--- src/lesson_5/script.js
+++ src/lesson_5/script.js
@@ -1,67 +1,72 @@
-// Вивести в console числа, від 10 до 25 з кроком 0.5 (10 й 25 включно)
-// for (let i = 10; i <= 25; i++) {
-//     console.log("i is equal to =", i)
-// }
-// Запитати та зберегти число введене із prompt.
-// const userNumber = prompt("Напищіть будь яке число");
-// console.log("userNumber =", userNumber);
+for (let i = 10; i <= 25; i +=0.5) {
+    console.log("i is equal to =", i);
+}
 
-// Визначати чи це число просте
-// let isPrime = "prime" ; 
+const userNumber = prompt("Напишіть будь яке число");
+console.log("userNumber =", userNumber);
 
-// for (var i = 2; i < userNumber; i++) {
-//     if (userNumber % 1 === 0) {
-//         isPrime = "not prime";
-//         break;
-//     }
-// }
+let isPrime = "prime"; 
 
-// console.log(`${userNumber} is ${isPrime}`);
+for (var i = 2; i < userNumber; i++) {
+    if (userNumber % 1 === 0) {
+        isPrime = "not prime";
+        break;
+    }
+}
 
-// Обрахувати знижку та повідомити користувачу значення знижки. Якщо сума покупки до 100 UAН - знижка 3%; якщо сума покупки до 200 UAH - знижка 5%; якщо сума покупки перевищує 200 UAH - знижка 7%
-// const userAmount = prompt("Яка сума вашої покупки?");
-// console.log("userAmount =", userAmount);
+console.log(`${userNumber} is ${isPrime}`);
 
+const userSum = Number(prompt("Яка сума вашої покупки?"));
+console.log("userAmount =", userSum);
 
+let discount = 0;
 
-/* Вивести таблицю множення чисел від 2 до 9 у зрозумілому форматі
+if (userSum <= 0) {
+  discount = null;
+} else if (userSum < 100) {
+  discount = 3;
+} else if (userSum < 200) {
+  discount = 5;
+} else if (userSum >= 200) {
+  discount = 7;
+} 
+
+ console.log( discount ? `Знижка ${discount}%` : 'Введіть позитивну суму');
+
 var result = '\n';
   for (var i = 2; i < 10; i++) {
     for (var j = 1; j < 11; j++) {
         result += (i*j) + ' ';
     }
-    result += '\n'
+    result += '\n';
 }
 
 console.log("result =", result);
-*/
 
-// Умова: один долар коштує 36,76 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів. Формат (10 USD = 3 676 UAH)
+const USD = 36.76;
 
-
-
-
-
-
-// Запитати у користувача 15 чисел і обрахувати, скільки серед них додатніх, від’ємних і нулів. Також визначатити кількість парних і непарних. Вивести статистику через console у зрозумілому форматі. *** Достатньо однієї змінної (не 15) для введення чисел користувачем.
-
-//const Numbers = prompt('введіть 15 чисел через пробел');
-//console.log("Numbers =", Numbers)
+for (let i = 10; i <= 100; i +=10) {
+    let total = i * USD;
+    total = total.toFixed(2);
+    total = new Intl.NumberFormat('ru-RU').format(total);
+    console.log(`(${i} USD = ${total} UAH)`);
+}
 
 let plus = 0;
 let minus = 0;
 let zero = 0;
-
 let even = 0;
 let odd = 0;
 
-
 for (let i=1; i < 16; i++) {
-  let userNumber = Number(prompt('Введіть 15 чисел')) ;
+  let userNumber = Number(prompt('Введіть 15 чисел'));
+
   if (userNumber > 0) {
     plus++;
+
   } else if (userNumber < 0) {
-    minus++;
+    minus++
+
   } else {
     zero++;
   }
@@ -70,22 +75,11 @@ for (let i=1; i < 16; i++) {
     even++;
   } else {
     odd++;
-  }
-
-// Спросить у пользователя 15 чисел и высчитать, сколько среди них положительных, отрицательных и нулей. 
-// Также определять количество четных и нечетных. Вывести статистику через console в понятном формате. **
-
-  
+  } 
 }
-console.log('положительных: ' + plus);
-console.log('negative: ' + minus);
-console.log('нуль: ' + zero);
 
-console.log('четное: ' + even);
-console.log('нечетное: ' + odd);
-
- 
-
-
-
-
+console.log('позитивних: ' + plus);
+console.log('негативних: ' + minus);
+console.log('нулеів: ' + zero);
+console.log('парне: ' + even);
+console.log('непарне: ' + odd);
