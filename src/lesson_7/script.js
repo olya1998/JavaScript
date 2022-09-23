40714e1c3f7dab770f0c949c06beb1ea0346b75d
Olya <olbka0844@gmail.com>
2022-09-23 02:21:58 +0200

    add carMileage function

--- src/lesson_7/script.js
+++ src/lesson_7/script.js
@@ -1,9 +1,49 @@
-// Cтворити функцію, яка створює та реєструє нові команду у автопробізі. Під час виклику функції потрібно передати дані про команду - назва команди, ім'я водія, рік народження водія, марка автомобіля. спонсор команди та чи водій допущений до автопробігу
+// Cтворити функцію, яка створює та реєструє нові команду у автопробізі. Під час виклику функції потрібно передати дані про команду - 
+// назва команди, - стричка
+// ім'я водія, стричка
+// рік народження водія, намбер
+// марка автомобіля.стричка
+// спонсор команди та стричка
+// чи водій допущений до автопробігу булевое
 
+
+function carMileage(teamName, driverName, year, carBrand, teamSponsor, isAllowed) {
+
+    // var booleanValue = true; 
+    // var numericalValue = 354;
+    // var stringValue = "This is a String";
+    // var stringObject = new String( "This is a String Object" );
+    // alert(typeof booleanValue) // displays "boolean"
+    // alert(typeof numericalValue) // displays "number"
+    // alert(typeof stringValue) // displays "string"
+    // alert(typeof stringObject) // displays "object"
+
+    if(typeof teamName !== 'string' || teamName.length < 3
+        || typeof driverName !== 'string' || driverName.length < 5
+        || typeof year !== 'number' ||  year < 1987
+        || typeof carBrand !== 'string' || carBrand.length < 4
+        || typeof teamSponsor !== 'string' || teamSponsor.length < 5
+        || typeof isAllowed !== 'boolean'
+    ){
+        console.log('wrong parameter');
+        return;
+    }
+    const team = {
+        teamName: teamName,
+        driverName: driverName,
+        year: year,
+        carBrand: carBrand,
+        teamSponsor: teamSponsor,
+        isAllowed: isAllowed,
+    };
+    return team;
+}
+
+ 
 
 // Запитати у користувача дані (через prompt та циклічно), про нового користувача у системі та зберегти його в об'єкт. Дані про користувача - логін, пароль, місто, країна, стать, вік
 
-const bert = prompt("Напишите свой логин, пароль, город");
+/*const bert = prompt("Напишите свой логин, пароль, город");
 console.log('your dsta =', bert)
 
 const userData = {
@@ -11,9 +51,10 @@ const userData = {
 }
 console.log(userData);
 
-//userData = bert;
+userData = bert;
+*/
 
-//  мои супер пупер изменения!
+
     
 
 
@@ -23,8 +64,8 @@ console.log(userData);
 // Створити об'єкт cтудента - name, surname, age, course, city, greeting, addHomework.  +
 // greetin - метод, котрий виводить повідомлення через console.log('Hi. everyone!'). addHomework - метод, котрий виводить повідомлення через console.log('Sending my howework... Please, wait') ???
 
-/*
-const student = {
+
+/*const student = {
     name: "Olya",
     surname: "Semenenko",
     age: 24,
@@ -33,17 +74,22 @@ const student = {
     greeting: 'Hi student!',
     addHomework: 'I will send soon, please wait',
 }
-console.log(student); */
+console.log(student); 
+console.log(student ["greeting"]); 
+console.log(student ["addHomework"]); 
+*/
 
 
 
 // Cтворити функцію isEmpty, яка повертає true, якщо об’єкт не має властивостей (порожній), інакше false.
 
 
+
+
 // Створити об’єкт для зберігання виручки команди продавців, наприкла: {Taras: 2000, Marta: 10 Ivan: 1200, Petro: 400, Roma: 366, Alina: 829} ??
 
-/*
-const salary = {
+
+const salaries = {
     Taras: 3000,
     Olha: 900,
     Mariya: 1500,
@@ -51,20 +97,34 @@ const salary = {
     Oleh: 3210,
     Petro: 899,
     
-}
-console.log(salary);
-*/
+};
+
+console.log(salaries);
+
 
 
 // Створити фукнцію, яка працює з цим обʼєктом та обчислює суму всіх виручок та виводить результат через сonsole.log
 
-/*
-function totalAmount () {
+function sumSalaries(salaries) {
 
-}
-*/
+    let sum = 0;
+    for (let salary of Object.values(salaries)) {
+      sum += salary;
+    }
+  
+    return sum; 
+  }
+  
+  
+  console.log( sumSalaries(salaries));
+
+
 
 // Створити фукнцію, яка працює з цим обʼєктом та яка знаходить продавця з найменшою виручкою та виводить результат через сonsole.log у зрозумілому форматі
+
+
+
+
 
 // Створити фукнцію, яка знаходить продавця з найбільшою виручкою та виводить результат через сonsole.log у зрозумілому форматі
 
