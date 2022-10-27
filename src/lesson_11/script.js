3abc643aa9653ff5e716ff0908f917307c69e5f9
Olya <olbka0844@gmail.com>
2022-10-28 01:02:58 +0200

    finalysing

--- src/lesson_11/script.js
+++ src/lesson_11/script.js
@@ -1,10 +1,3 @@
-// Створити сторінку, що буде показувати список продуктів для купівлі
-
-// Cписок продуктів для купівлі - масив, кожен елемент якого представлений нижче кодом
-
-// Довжина такого масиву - 15
-
- 
 const productList = [
     {
         productName: 'bread',
@@ -34,10 +27,10 @@ const productList = [
         productName: 'avocado ',
         productData: {
             weight: 30,
-            certificate: true,
+            certificate: false,
             dateofExpiry: '15/10/2022',
             sugarFree: true,
-    },
+        },
         productProvider: 'FOP Class',
         productCountry: 'Ukraine',
         productPrice: 45.16,
@@ -49,7 +42,7 @@ const productList = [
             certificate: true,
             dateofExpiry: '23/10/2022',
             sugarFree: false,
-    },
+        },
         productProvider: 'FOP Posad',
         productCountry: 'SouthAfrica',
         productPrice: 25.14,
@@ -61,7 +54,7 @@ const productList = [
             certificate: true,
             dateofExpiry: '20/10/2022',
             sugarFree: false,
-    },
+        },
         productProvider: 'FOP Owoci',
         productCountry: 'India',
         productPrice: 85.94,
@@ -70,10 +63,10 @@ const productList = [
         productName: 'blackberry',
         productData: {
             weight: 150,
-            certificate: true,
+            certificate: false,
             dateofExpiry: '15/10/2022',
             sugarFree: false,
-    },
+        },
         productProvider: 'FOP ATB',
         productCountry: 'Poland',
         productPrice: 55.33,
@@ -85,7 +78,7 @@ const productList = [
             certificate: true,
             dateofExpiry: '21/10/2022',
             sugarFree: true,
-    },
+        },
         productProvider: 'FOP ATB',
         productCountry: 'Ukraine',
         productPrice: 10.00,
@@ -97,7 +90,7 @@ const productList = [
             certificate: true,
             dateofExpiry: '12/06/2022',
             sugarFree: true,
-    },
+        },
         productProvider: 'FOP CLASS',
         productCountry: 'Ukraine',
         productPrice: 94.99,
@@ -106,10 +99,10 @@ const productList = [
         productName: 'yogurt',
         productData: {
             weight: 90,
-            certificate: true,
+            certificate: false,
             dateofExpiry: '10/10/2022',
             sugarFree: false,
-    },
+        },
         productProvider: 'FOP SLASTENA',
         productCountry: 'Ukraine',
         productPrice: 16.87,
@@ -121,7 +114,7 @@ const productList = [
             certificate: true,
             dateofExpiry: '21/10/2022',
             sugarFree: false,
-    },
+        },
         productProvider: 'FOP Fruits',
         productCountry: 'Brazil',
         productPrice: 30.99,
@@ -133,7 +126,7 @@ const productList = [
             certificate: true,
             dateofExpiry: '21/10/2022',
             sugarFree: true,
-    },
+        },
         productProvider: 'FOP ATB',
         productCountry: 'Ukraine',
         productPrice: 75.99,
@@ -142,10 +135,10 @@ const productList = [
         productName: 'carrot',
         productData: {
             weight: 200,
-            certificate: true,
+            certificate: false,
             dateofExpiry: '16/10/2022',
             sugarFree: false,
-    },
+        },
         productProvider: 'FOP ATB',
         productCountry: 'Ukraine',
         productPrice: 12.14,
@@ -157,7 +150,7 @@ const productList = [
             certificate: true,
             dateofExpiry: '01/09/2022',
             sugarFree: true,
-    },
+        },
         productProvider: 'FOP ATB',
         productCountry: 'Ukraine',
         productPrice: 14.44,
@@ -166,10 +159,10 @@ const productList = [
         productName: 'oil',
         productData: {
             weight: 500,
-            certificate: false,
+            certificate: true,
             dateofExpiry: '21/10/2022',
             sugarFree: true,
-    },
+        },
         productProvider: 'FOP ATB',
         productCountry: 'Ukraine',
         productPrice: 35.55,
@@ -181,73 +174,53 @@ const productList = [
             certificate: false,
             dateofExpiry: '01/03/2022',
             sugarFree: true,
-    },
+        },
         productProvider: 'ASHAN',
         productCountry: 'China',
         productPrice: 43.00,
     },
 ];
 
-// const body = document.getElementsByTagName('body')[0];
-// body.style.color = 'red'
-
-
 const ul = document.createElement('ul');
 
-document.body.append(ul)
+document.body.append(ul);
+let summary = 0;
+let max = 0;
 
 productList.forEach((element) => {
-// 
+    summary = summary + element.productPrice;
+    max = element.productPrice > max ? element.productPrice : max;
+
     const li = document.createElement('li');
     const certificate = element.productData.certificate ? '' : '<i class="fa fa-exclamation" aria-hidden="true"></i>';
-    const sugar = element.productData.sugarFree ? '<img src="./icons/sugar.png" width = " 24 " alt="">' : '<img src="./icons/sugar-free.png" width = " 24 " alt="">'
-    const flag = `<img src="./icons/${element.productCountry}.png" width = " 14 " alt="">`;
+    
+    const sugarValue = element.productData.sugarFree ? 'sugar' : 'sugar-free'; 
+    const sugar = `<img src="./icons/${sugarValue}.png" width = "24">`;
+    
+    const flag = `<img src="./icons/${element.productCountry}.png" width = "24">`;
 
+    const date = `<span class="date">${element.productData.dateofExpiry}</span>`;
 
+    const productProvider = `<span class="provider">${element.productProvider}</span>`;
 
-    li.innerHTML = ` ${certificate} ${sugar} ${element.productName} : weight ${element.productData.weight}, date of expiry: ${element.productData.dateofExpiry}, ${flag}, price: ${element.productPrice}`;
+    li.innerHTML = `${certificate} ${sugar} ${element.productName} : weight ${element.productData.weight} , date of expiry: ${date} , ${productProvider} ${flag} price: ${element.productPrice}`;
 
     ul.append(li);
   });
 
+  const avg = (summary / productList.length).toFixed(2);
+  const summaryDiv = document.createElement('div');
 
+  summaryDiv.classList.add('summary');
+  summaryDiv.innerHTML = `Total summary: ${summary}`;
+  document.body.append(summaryDiv);
 
-/* {
-    productName: 'bread',
-    productData: {
-        weight: 200,
-        certificate: true,
-        dateofExpiry: '21/10/2022',
-        sugarFree: true,
-    },
-    productProvider: 'FOP ATB',
-    productCountry: 'Ukraine',
-    productPrice: 12.14,
-}, */
+  const avgDiv = document.createElement('div');
+  avgDiv.classList.add('avg');
+  avgDiv.innerHTML = `Average price: ${avg}`;
+  document.body.append(avgDiv);
 
-
-
-
-
-
-
-
-
-// // 1.найтип доступ к node 2. посмотреть по уроку или гугл, как добавлять элементы в базовый узел (в ноду) 3. Пройти циклом по массиву объектов в котором внешние края ul а внутренние li
-
-
-// Сформувати список покупок у вигляді списку (<ul> з <li>)
-
-// Кожний парний рядок повинен бути стилізований із backgroundColor, щоб чітко виділявся й було зручно читати. Колір - довільний
-
-// productCountry - повинен бути <img /> із відповідним прапором
-
-// Якщо товар не сертифікований - додавати стилізований знак оклику (!), щоб користувач звернув увагу
-
-// Додати стилі до такого списку покупок. Якомога більше, без конкретних вимог
-
-// Додати будь-який HTML-тег, в якого записати вартість всього продуктового кошика
-
-// Додати будь-який HTML-тег, в якого записати найдорожчий із продуктів цього кошика
-
-// Додати будь-який HTML-тег, в якого записати середню ціну по продуктах
+  const maxDiv = document.createElement('div');
+  maxDiv.classList.add('max');
+  maxDiv.innerHTML = `Maximum price: ${max}`;
+  document.body.append(maxDiv);
