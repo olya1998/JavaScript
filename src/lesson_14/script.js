ea1c670ffc59932f6992511cf2f208e755040f35
Olya <olbka0844@gmail.com>
2022-11-08 21:30:41 +0100

    add clear homework

--- src/lesson_14/script.js
+++ src/lesson_14/script.js
@@ -1,67 +1,114 @@
-// Вирішити алгоритмічні задачки
-// Всі дані для тесту задачок отримувати через input-поля з HTML
-// Біля кожного input-поля кнопка "Вирішити задачу 1" (2,3,4)
-
-/*
-1) Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation. 
-   Возвращает век введенного года. Ввод всегда будет строкой из 4 цифр, поэтому проверка не требуется.
-
-
-"1999" --> "20th"
-"2011" --> "21st"
-"2154" --> "22nd"
-*/
-
 const input = document.querySelector('#year');
 const btn1 = document.querySelector('#task_1');
 
 btn1.addEventListener('click', function () {
-    
     const century = Math.trunc((input.value / 100) + 1);
 
     const pastNumber = century % 10;
     let ends;
-    if(pastNumber ===1 ) {
+    if(pastNumber === 1) {
         ends = 'st';
     } else if(pastNumber === 2) {
         ends = 'nd';
     } else if(pastNumber === 3) {
-        ends = 'rd'
+        ends = 'rd';
     } else {
-        ends = 'th'
+        ends = 'th';
     }
-    console.log(century + ends);
 
+    const centuryDiv = document.getElementById('century');
+    centuryDiv.innerHTML = `It is ${century + ends} century`;
 });
-/*
-2) Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's. All non-vowels including non alpha characters (spaces,commas etc.) should be included.
-Напишите функцию, которая принимает строку и выводит строки из 1 и 0, где гласные становятся 1, а не гласные — 0. Должны быть включены все негласные, включая небуквенные символы (пробелы, запятые и т. д.).
 
-vowelOne( "abceios" ) // "1001110"
-vowelOne( "aeiou, abc" ) // "1111100100"
-*/
+function vowelOne(string) {
+    const vowels = ['a','A', 'e', 'E', 'i', 'I', 'o', 'O','u', 'U', 'y', 'Y'];
+    let resultString = '';
 
-/*
-3) Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
-Напишите функцию, которая принимает строку из одного или нескольких слов и возвращает ту же строку, но с перевернутыми словами из пяти или более букв. Передаваемые строки будут состоять только из букв и пробелов. Пробелы будут включены только в том случае, если присутствует более одного слова.
+    Array.from(string)
+        .forEach(function(char){
+            resultString += vowels.includes(char)? '1' : '0';
+    });
 
-spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
-spinWords( "This is a test") => returns "This is a test" 
-spinWords( "This is another test" )=> returns "This is rehtona test"
-*/
+    return resultString;
+}
 
-/*
-4) You are given a string of space separated numbers, and have to return the highest and lowest number.
-Вам дается строка чисел, разделенных пробелами, и вы должны вернуть наибольшее и наименьшее число.
+const btn_2 = document.getElementById('task_2');
 
-highAndLow("1 2 3 4 5");  // return "5 1"
-highAndLow("1 2 -3 4 5"); // return "5 -3"
-highAndLow("1 9 3 4 -5"); // return "9 -5"
-*/
+btn_2.addEventListener('click', function () {
+    const input_2 = document.getElementById('input_2').value;
+    const resultDiv_2 = document.getElementById('result_2');
 
-/*
-5) Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
-Напишите функцию, которая принимает массив из 10 целых чисел (от 0 до 9), которая возвращает строку этих чисел в виде номера телефона.
+    resultDiv_2.innerHTML = vowelOne(input_2); 
+});
 
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
-*/
+function spinWords(sting) {
+    sting.trim();
+
+    const words = sting.split(' ');
+    const resulArray = [];
+
+    words.forEach(function (word) {
+       if(word.length >= 5) {
+           const splitString = word.split('');
+           const reversed = splitString.reverse();
+           word = reversed.join('');
+       }
+           resulArray.push(word);
+    });
+
+     return resulArray.join(' ');
+}
+
+const btn_3 = document.getElementById('task_3');
+
+btn_3.addEventListener('click', function () {
+    const input_3 = document.getElementById('input_3').value;
+    const resultDiv_3 = document.getElementById('result_3');
+
+    resultDiv_3.innerHTML = spinWords(input_3);
+});
+
+function highAndLow(string) {
+    string.trim();
+    const numbers = [];
+
+    const words = string.split(' ');
+    words.forEach(function (number) {
+        numbers.push(+number)
+    });
+
+    return {
+        min: Math.min(...numbers),
+        max: Math.max(...numbers),
+    };
+}
+
+const btn_4 = document.getElementById('task_4');
+
+btn_4.addEventListener('click', function () {
+    const input_4 = document.getElementById('input_4').value;
+    const resultDiv_4 = document.getElementById('result_4');
+
+    const results = highAndLow(input_4)
+    resultDiv_4.innerHTML = `min: ${results.min}, max: ${results.max}`;    
+});
+
+function createPhoneNumber(arrayOfNumbers) {
+    const stringNumber = arrayOfNumbers.join('');
+    const code = stringNumber.substring(0, 3);
+    const firstPart = stringNumber.substring(3, 6);
+    const secondPart = stringNumber.substring(6, 10);
+
+    return `(${code}) ${firstPart}-${secondPart}`;
+}
+
+const btn_5 = document.getElementById('task_5');
+
+btn_5.addEventListener('click', function () {
+    const input_5 = document.getElementById('input_5').value;
+    const resultDiv_5 = document.getElementById('result_5');
+
+    const numbers = input_5.split(' ');
+
+    resultDiv_5.innerHTML = createPhoneNumber(numbers);   
+});
