f754918ff2088c98670a7f1dd6333c41c3ee12ab
Olya <olbka0844@gmail.com>
2022-12-10 01:02:25 +0100

    add clear JS

--- src/lesson_17/script.js
+++ src/lesson_17/script.js
@@ -4,10 +4,11 @@
 
 const apiKey = '1dB5HAESsayMuxns9csvyV9lpA7Bhe5L';
 const baseURL = 'http://dataservice.accuweather.com';
-
 const inputCity = document.getElementById('city');
-//
 const cityName = document.getElementById('caption');
+const getWeather = document.getElementById('get_weather');
+const neighborsDiv = document.getElementById('neighbors');
+const resultDiv = document.getElementById('weather_result');
 
 
 function getWeatherURLI (cityKey) {
@@ -21,27 +22,6 @@ function getSearchrURI (city) {
 function getNeighborsURI (cityKey) {
     return baseURL + `/locations/v1/cities/neighbors/${cityKey}?apikey=${apiKey}`;
 }
-// /locations/v1/cities/neighbors/324561?apikey=1dB5HAESsayMuxns9csvyV9lpA7Bhe5L
-
-const citiesList = {
-    'Lviv': 324561,
-    'Kyiv': 324505,
-    'Kharkiv': 323903,
-    'Chernihiv': 322162,
-    'Odesa': 325343,
-    'Mariupol': 323037,
-    'Poltava': 325523,
-    'Zhytomyr': 326609,
-    'Cherkasy': 321985,
-    'London': 328328,
-    'Warsaw': 274663,
-    'New York': 349727,
-};
-
-const getWeather = document.getElementById('get_weather');
-//
-const neighborsDiv = document.getElementById('neighbors');
-const xhttp = new XMLHttpRequest();
 
 function formatDate(date) {
     const yyyy = date.getFullYear();
@@ -72,19 +52,9 @@ function renderHtml(daysData) {
         </div>`;
 }
 
-//searchCity
-
 function searchCity(cityName) {
-    console.log(cityName);
-    // наверно лучше не сам ключ, а название города передавать
-    // чтобы потом по названию искать 
-    // так не очень оптимально - но мельше гемороя 
-// меняем значение в строке поиска
-inputCity.value = cityName;
-
-getWeatherData();
-
-
+    inputCity.value = cityName;
+    getWeatherData();
 }
 
 function getNeighborCityLink(cityObject) {
@@ -94,26 +64,28 @@ function getNeighborCityLink(cityObject) {
     </div>`;
 }
 
-const resultDiv = document.getElementById('weather_result');
+function cleatResults() {
+    resultDiv.innerHTML = '';
+    cityName.innerHTML = '';
+    neighborsDiv.innerHTML = '';
+}
+
 
 async function getWeatherData() {
-    //const inputCity = document.getElementById('city').value;
-    //чистим старые результаты
-
-
-
-    // делаем запрос к серверу - ищем такой город
     const cityResult = await fetch(getSearchrURI(inputCity.value))
     .then((response) => response.json())
     .catch((error) => {
         console.error('Error:', error);
     });
 
-
     //если не находим - сообщение об ошибке
     if(!cityResult.length) {
         console.log('Error')
+        // тут не сделали вывод - что город не нейден 
         // вывести сообщение что такого города нет - пользователю - какой-то див с этим текстом
+        cleatResults();
+        resultDiv.innerHTML = `<h3> City not found </h3>`;
+
         return;  // это прекращает дальнейшее выволнение функции
     }
 
@@ -127,13 +99,7 @@ async function getWeatherData() {
     .catch((error) => {
         console.error('Error:', error);
     });
-
-     console.log(neighborsResult);
-    // return;
-
     
-
-
 
     // делаем запрос прогоды по этому городу
     const weatherResult = await fetch(getWeatherURLI(searchCityKey))
@@ -142,7 +108,6 @@ async function getWeatherData() {
         console.error('Error:', error);
     });
    
-
 
     // отрисовываем погоду по этому городу 
     resultDiv.innerHTML = '';
@@ -154,7 +119,7 @@ async function getWeatherData() {
     });
 
     // и создаём кнопки - соседние города 
-    //getNeighborCityLink
+
 
     //если не находим соседей - в области, где должны быть ссылки на другие города - надпись - соседей не нашли
     if(neighborsResult.length) {
@@ -169,73 +134,17 @@ async function getWeatherData() {
 
     } else {
         console.log('Error:  no neighbors');
-        // вывести сообщение соседей не нашли - какой-то див с этим текстом
         // продолжаем выполнение, так как это не меняет основного функционала 
     }
 
-   
+       // обработчик клика на этот город - подставляется значение в строку поиска и все шаги повторяются 
 
-    return ;
-    // обработчик клика на этот город - подставляется значение в строку поиска и все шаги повторяются 
-
-    if(citiesList.hasOwnProperty(inputCity)) {
-        xhttp.open('Get', `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${citiesList[inputCity]}?apikey=${apiKey}`);
-        xhttp.send();
-        xhttp.onload = function() {
-            if(this.readyState === 4 && this.status === 200) {
-
-                const data = JSON.parse(this.response);
-                
-                resultDiv.innerHTML = '';
-
-                data.DailyForecasts.forEach(function(day) {
-                    const dayHtml = renderHtml(day);
-
-                    resultDiv.innerHTML += dayHtml;
-                });
-
-            } else {
-                console.log(JSON.parse(this.response));
-
-                return null;
-            }
-        };
- 
-    } else {
-     console.log('ERROR!');
-     resultDiv.innerHTML = '<h1 id="error">Error: City is Not Valid!</h1>';
-    }    
 }
 
 getWeather.addEventListener('click', getWeatherData);
 
-async function showWeather() {
+  
 
-    // 
-    let response = await fetch('/article/promise-chaining/user.json');
-    let user = await response.json();
-  
-    // запрашиваем информацию об этом пользователе из github
-    let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
-    let githubUser = await githubResponse.json();
-  
-    // отображаем аватар пользователя
-    let img = document.createElement('img');
-    img.src = githubUser.avatar_url;
-    img.className = "promise-avatar-example";
-    document.body.append(img);
-  
-    // ждём 3 секунды и затем скрываем аватар
-    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-  
-    img.remove();
-  
-    return githubUser;
-  }
-  
-  //showWeather();
-
-// Переписати ввесь робочий код під async/await
 
 // ***Забрати обмеження в кількість міст. Відтепер можна вводити будь-яке місто/селище тощо, й якщо API повертає результат - відмальовувати його. (стосується тільки тих, в кого місто треба вводити у input-поле)
 
